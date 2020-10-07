43ad9b997cb149abfd221871d69f85fe96ea060e
chxiaoyu <961041421@qq.com>
2020-10-07 17:53:33 +0800

    完成登录请求，并判断是否登录成功

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -29,7 +29,7 @@
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
           <el-button type="info" @click="resetloginForm">重置</el-button>
         </el-form-item>
       </el-form>
@@ -61,8 +61,27 @@ export default {
     }
   },
   methods: {
+    // 表单重置
     resetloginForm() {
       this.$refs.loginFormRef.resetFields()
+    },
+    // 登录
+    login() {
+      // 登录校验表单，并发送登录请求和详情结果判断：
+      // 由于 POST 返回的是一个 promise ，为了简化操作可以用 async await 来进行优化；
+      // 同时 await 只能用在 async 修饰的方法中，需要将 validate 中的箭头函数改写成异步的方法，这样就能拿到服务器直接返回的数据；
+      // 由于服务器返回的属性中 data 属性中的数据才是真实数据，因此采用解构赋值的将 data 解构出来赋值给 res；
+      // 最后在通过 res 中的 meta 属性中的返回状态码进行判断是否登录成功
+      this.$refs.loginFormRef.validate(async valid => {
+        // console.log(valid)
+        if (!valid) return false
+        const { data: res } = await this.$api.post('login', this.loginForm)
+        console.log(res)
+        if (res.meta.status === 200) {
+          console.log('登录成功')
+        }
+        console.log('登录失败')
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 
+// 导入 axios 并挂载到根实例上，设置访问基础地址路径
+import axios from 'axios'
+Vue.prototype.$api = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
 Vue.config.productionTip = false
 
 new Vue({
