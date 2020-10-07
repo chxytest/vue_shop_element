cec21c680f35b93181fccb1094eca18291542294
chxiaoyu <961041421@qq.com>
2020-10-07 16:38:06 +0800

    完成表单的样式和数据绑定

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,98 @@
 <template>
-  <div>登录组件</div>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 登录头像 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+
+      <!-- 登录表单 -->
+      <el-form label-width="0" class="login_form" :model="loginForm">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input placeholder="用户名" prefix-icon="el-icon-user-solid" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input
+            placeholder="密码"
+            prefix-icon="el-icon-lock"
+            v-model="loginForm.password"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.login_container {
+  background: #2b4b6b;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login_box {
+  height: 300px;
+  width: 450px;
+  background: #fff;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  position: relative;
+
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    top: -25%;
+    background-color: #fff;
+
+    > img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
