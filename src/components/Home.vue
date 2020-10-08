bb11ab1ab1191d0e448795759ce4cddf5363eaaf
chxiaoyu <961041421@qq.com>
2020-10-08 10:47:02 +0800

    完成home页header部分的样式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>
+      <div class="header-left">
+        <img src="../assets/heima.png" alt />
+        <span>电商项目后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -9,7 +19,9 @@ export default {
   name: 'Home',
   methods: {
     logout() {
+      // 清空token
       window.sessionStorage.clear()
+      // 跳转到登录页
       this.$router.push('/login')
     }
   }
@@ -17,4 +29,34 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 0;
+  color: #fff;
+  font-size: 20px;
+
+  .header-left {
+    display: flex;
+    align-items: center;
+
+    > span {
+      padding-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
