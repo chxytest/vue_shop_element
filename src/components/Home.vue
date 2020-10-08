3748d82024cb224697ab6ced9ee821c23c4fac46
chxiaoyu <961041421@qq.com>
2020-10-08 10:15:34 +0800

    完成退出功能按钮

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>首页</div>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
