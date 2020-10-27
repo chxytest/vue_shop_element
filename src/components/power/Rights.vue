de57e4f92a3ba2829bf9e2fcc1e9f8a516c00556
chxiaoyu <961041421@qq.com>
2020-10-27 22:24:20 +0800

    新建权限组件，并完成组件的基础结构

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- 面包屑路径 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 用户列表卡片区域 -->
+    <el-card>123</el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Rights'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
+import Rights from '../components/power/Rights.vue'
 
 Vue.use(VueRouter)
 
@@ -16,7 +17,8 @@ const routes = [
     redirect: '/welcome',
     children: [
       { path: '/welcome', component: Welcome },
-      { path: '/users', component: Users }
+      { path: '/users', component: Users },
+      { path: '/rights', component: Rights }
     ]
   }
 ]
