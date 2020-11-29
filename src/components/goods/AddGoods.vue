ed7f37a8c4833dff051e221258ffeaf5b7f2577f
chxiaoyu <961041421@qq.com>
2020-11-29 19:08:15 +0800

    完成添加商品中tab标签下信息、参数和属性的内容

--- src/components/goods/AddGoods.vue
+++ src/components/goods/AddGoods.vue
@@ -1,17 +1,232 @@
 <template>
-  <div>添加商品页面</div>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片区域 -->
+    <el-card>
+      <!-- 提示信息区域 -->
+      <el-alert title="添加商品信息" type="info" center show-icon :closable="false"></el-alert>
+
+      <!-- 进度条区域 -->
+      <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+
+      <!-- 纵向tab标签栏 -->
+      <el-form
+        :model="addGoodsForm"
+        :rules="addGoodsFormRules"
+        ref="addGoodsFormRef"
+        label-width="100px"
+        label-position="top"
+      >
+        <el-tabs
+          v-model="activeIndex"
+          :tab-position="'left'"
+          :before-leave="beforeTabLeave"
+          @tab-click="tabClick"
+        >
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addGoodsForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addGoodsForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addGoodsForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addGoodsForm.goods_number" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类" prop="goods_cat">
+              <el-cascader
+                v-model="addGoodsForm.goods_cat"
+                :options="goodsCategoriesList"
+                :props="goodsProps"
+                @change="handleChange"
+              ></el-cascader>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">
+            <el-form-item :label="item.attr_name" v-for="item in manyTableData" :key="item.attr_id">
+              <el-checkbox-group v-model="item.attr_vals">
+                <el-checkbox
+                  :label="cb"
+                  v-for="(cb, i) in item.attr_vals"
+                  :key="i"
+                  border
+                  size="small"
+                ></el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">
+            <el-form-item :label="item.attr_name" v-for="item in onlyTableData" :key="item.attr_id">
+              <el-input v-model="item.attr_vals"></el-input>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'AddGoods',
   data() {
-    return {}
+    return {
+      activeIndex: '0', // 激活的步骤
+      addGoodsForm: {
+        // 添加商品的表单信息
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0,
+        goods_cat: []
+      },
+      addGoodsFormRules: {
+        // 商品表单的校验规则
+        goods_name: [
+          {
+            required: true,
+            message: '请输入商品名',
+            trigger: 'blur'
+          }
+        ],
+        goods_price: [
+          {
+            required: true,
+            message: '请输入商平价格',
+            trigger: 'blur'
+          }
+        ],
+        goods_weight: [
+          {
+            required: true,
+            message: '请输入商平重量',
+            trigger: 'blur'
+          }
+        ],
+        goods_number: [
+          {
+            required: true,
+            message: '请输入商平重量',
+            trigger: 'blur'
+          }
+        ],
+        goods_cat: [
+          {
+            required: true,
+            message: '请选择商品分类',
+            trigger: 'blur'
+          }
+        ]
+      },
+      goodsCategoriesList: [], // 商品分类列表数据
+      goodsProps: {
+        // 级联选择权参数
+        expandTrigger: 'hover',
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      },
+      manyTableData: [], // 商品动态参数分类列表数据
+      onlyTableData: [] // 商品静态属性分类列表数据
+    }
   },
-  created() {},
-  methods: {}
+  created() {
+    this.getGoodsCategories()
+  },
+  computed: {
+    categoriesId() {
+      return this.addGoodsForm.goods_cat.length === 3
+        ? this.addGoodsForm.goods_cat[2]
+        : null
+    }
+  },
+  methods: {
+    // 1、请求商品分类数据
+    async getGoodsCategories() {
+      const { data: res } = await this.$api.get('categories')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类数据失败')
+      }
+      this.goodsCategoriesList = res.data
+      console.log(this.goodsCategoriesList)
+    },
+    // 2、监听级联选择器中的变化
+    handleChange() {
+      console.log(this.addGoodsForm.goods_cat)
+      if (this.addGoodsForm.goods_cat.length !== 3) {
+        this.addGoodsForm.goods_cat = []
+      }
+    },
+    // 3、判断标签的切换
+    beforeTabLeave() {
+      if (
+        this.activeIndex === '0' &&
+        this.addGoodsForm.goods_cat.length !== 3
+      ) {
+        this.$message.error('请选择商品分类！')
+        return false
+      }
+    },
+    // 4、tab 标签切换事件触发
+    async tabClick() {
+      console.log(this.activeIndex)
+      if (this.activeIndex === '1') {
+        const { data: res } = await this.$api.get(
+          `categories/${this.categoriesId}/attributes`,
+          {
+            params: { sel: 'many' }
+          }
+        )
+        if (res.meta.status !== 200) {
+          return this.$message.error('获取动态参数列表数据失败！')
+        }
+        res.data.forEach(item => {
+          item.attr_vals =
+            item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
+        })
+        this.manyTableData = res.data
+        console.log(this.manyTableData)
+      } else if (this.activeIndex === '2') {
+        const { data: res } = await this.$api.get(
+          `categories/${this.categoriesId}/attributes`,
+          {
+            params: { sel: 'only' }
+          }
+        )
+        if (res.meta.status !== 200) {
+          return this.$message.error('获取静态属性列表数据失败！')
+        }
+        this.onlyTableData = res.data
+        console.log(this.onlyTableData)
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.el-cascader {
+  width: 300px;
+}
+.el-checkbox {
+  margin: 0 10px 0 0 !important;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -32,7 +32,11 @@ import {
   Cascader,
   Alert,
   Tabs,
-  TabPane
+  TabPane,
+  Steps,
+  Step,
+  CheckboxGroup,
+  Checkbox
 } from 'element-ui'
 
 Vue.use(Button)
@@ -66,5 +70,10 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
